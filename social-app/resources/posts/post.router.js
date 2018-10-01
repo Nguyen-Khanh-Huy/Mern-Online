497408b85457bed2cf5e31c3a4289d84ba614de2
Huy <43417013+Nguyen-Khanh-Huy@users.noreply.github.com>
2018-10-01 23:11:50 +0700

    test

--- social-app/resources/posts/post.router.js
+++ social-app/resources/posts/post.router.js
@@ -8,3 +8,14 @@ postRouter.get('/:id', postController.getById);
 postRouter.post('/', postController.create);
 postRouter.put('/:id', postController.updatePost);
 postRouter.delete('/:id', postController.deletePost);
+
+
+
+/*
+{
+    "content": "admin1",
+    "imageUrl": "hinh1",
+    "likes" : ["5bb089ac27c7ed2ca81d90f1"],
+    "postBy" : "5bb08a9827c7ed2ca81d90f2"
+}
+*/
